f9adaf1a1dd6d748502a56f5b20f34ec49c07be4
aditya28031 <aditya28031@gmail.com>
2024-02-17 22:56:55 +0530

    Code uploaded till Session 65

---
+++ MSTC_04/Self/DAY1/Practice1.c
@@ -0,0 +1,329 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <math.h>
+#include <math.h>
+#include <math.h>
+#include <math.h>
+-----------------------------------------------------------------------------------------------------------
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+[1]
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+[2]
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int k;
+int l;
+int m;
+int n;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+[3]
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int n;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+[4]
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int n;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+[5]
+int a;
+int b;
+int c;
+int d;
+int e;
+int f;
+int g;
+int h;
+int i;
+int j;
+int k;
+int l;
+int m;
+int n;
+int o;
+int p;
+int q;
+int s;
+int t;
+int u;
+int v;
+int w;
+int x;
+int y;
+int z;
+-------------------------------------------------------------------------------------------------------------------------
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size: ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i= 0; i < size; ++i);
+for(int i = 0 ; i < size ; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i= 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0 ;i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < siz ; ++i);
+for(int i = 0; i < size; i++);
+for(int i = 0; i < size; i++);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < sise; ++i);
+for(int i= 0; i < size;++i);
+for(int i= 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0l i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i  < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
+for(int i = 0; i < size; ++i);
