5646a4864c5e4f3e70f462cb154e6efa96e82f8d
Aditya Darekar <aditya28301@gmail.com>
2024-06-22 23:07:36 +0530

    Hash_Queue

--- C Programming And DSA/Hash Queue/Hash_Queue.c
+++ C Programming And DSA/Hash Queue/Hash_Queue.c
@@ -1,260 +1,272 @@
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 
-#define TRUE    1 
-#define FALSE   0
-#define SUCCESS 1 
- 
-typedef unsigned long long int key_t; 
-typedef unsigned long long int index_t; 
+#define TRUE 1
+#define FALSE -1
 
-typedef struct hash_queue hash_queue_t; 
-typedef struct node node_t; 
-typedef node_t list_t; 
+#define KEY 10000
+#define BUCKET_SIZE 1000
 
-typedef int status_t; 
+typedef struct node node_t;
+typedef node_t list_t;
+typedef int status_t;
+typedef struct hash_queue hash_queue_t;
+typedef unsigned long long int index_t;
+typedef unsigned long long int key_t;
 
 struct node{
-    key_t key; 
-    struct node* prev; 
-    struct node* next; 
-}; 
+    key_t key;
+    node_t* next;
+    node_t* prev;
+};
 
 struct hash_queue{
-    list_t* p_list_head_array; 
-    size_t bucket_size; 
-}; 
+    node_t** p_list_array;
+    size_t bucket_size;
+};
 
-/* Hash queue interface functions */
-hash_queue_t** create_hash_queue(size_t bucket_size); 
-void add_key(hash_queue_t** p_hash_queue, key_t key); 
-//status_t remove_key(hash_queue_t* p_hash_queue, key_t key); 
-status_t search_key(hash_queue_t* p_hash_queue, key_t key); 
-void show_hash_queue(hash_queue_t** p_hash_queue); 
-status_t destroy_hash_queue(hash_queue_t*** p_hash_queue); 
+//Hash Queue interface functions
+hash_queue_t* create_hash_queue(size_t bucket_size);
+void add_key(hash_queue_t* hash_queue, key_t key);
+void show_hash_queue(hash_queue_t* hash_queue);
+status_t search_key(hash_queue_t* hash_queue, key_t key);
+status_t remove_key(hash_queue_t* hash_queue, key_t key);
+void destroy_hash_queue(hash_queue_t* hash_queue);
 
-/* Hash queue helper function */
-static index_t hash(key_t key, size_t bucket_size);
+// Hash Queue Helper function
+index_t hash(key_t key, size_t bucket_size);
 
-/* Linked list interface functions */
-list_t* create_list(void); 
-status_t insert_end(list_t* p_list, key_t key); 
-status_t remove_key(list_t* p_list, key_t key); 
-status_t find(list_t* p_list, key_t key); 
-
-/* Linked list helper functions */
-static void generic_insert(node_t* p_beg, node_t* p_mid, node_t* p_end); 
-static void generic_delete(node_t* p_delete_node); 
-static node_t* search_node(list_t* p_list, key_t key); 
-static node_t* get_node(key_t key); 
+// Linked List Helper function
+static void generic_insert(list_t* p_beg, list_t* p_mid, list_t* p_end);
+static list_t* create_list();
+static list_t* get_node(key_t key);
 static void* xmalloc(size_t size_in_bytes);
-void show_list(list_t* p_node); 
-void destroy(list_t* l);
+static list_t* find_search_node(list_t* list, key_t key);
+static status_t generic_delete(list_t* p_beg, list_t* p_mid, list_t* p_end);
+
+// Linked List interface Function 
+void insert_end(node_t* list, key_t key);
+void show_list(list_t* list);
+status_t search_node(list_t* list, key_t key);
+status_t remove_node(list_t* list, key_t key);
+static void destroy_list(list_t** list);
 
 int main(void){
+    hash_queue_t* hash_queue = NULL;
+    status_t status;
 
-    size_t bucket_size = 5;
-    int status;
+    hash_queue = create_hash_queue(BUCKET_SIZE);
 
-    hash_queue_t** pp_hash_q =  create_hash_queue(bucket_size);
-    add_key(pp_hash_q, 10);
-    add_key(pp_hash_q, 19);
-    add_key(pp_hash_q, 27);
-    add_key(pp_hash_q, 37);
-    
-    show_hash_queue(pp_hash_q);
-    status = destroy_hash_queue(&pp_hash_q);
-    assert(status != EXIT_FAILURE);
-    if(pp_hash_q == NULL)
-        puts("Executed success");
+    for(index_t index = 0; index < KEY; ++index)
+        add_key(hash_queue, index);
 
-    return  (EXIT_SUCCESS);  
-}
+    show_hash_queue(hash_queue);
 
-/* Hash queue interface functions */
-hash_queue_t** create_hash_queue(size_t bucket_size){
-    size_t i;
-    hash_queue_t** aHash_q = NULL;
-    aHash_q = (hash_queue_t**)malloc((bucket_size) * sizeof(hash_queue_t*));
-
-    if (aHash_q == NULL) {
-        perror("Failed to allocate memory for hash queue array");
-        return NULL;
+    printf("\nHash Queue Data Searching ...\n");
+    for(index_t index = 0; index < KEY+1; ++index){
+        status = search_key(hash_queue, index);
+        if(status == TRUE)
+            printf("KEY = [%llu] is present in Hash_Queue\n", index);
+        else    
+            printf("KEY = [%llu] is not present in Hash_Queue\n", index);
     }
 
-    for(i = 0; i < bucket_size; ++i){
-        aHash_q[i] = (hash_queue_t*)malloc(sizeof(hash_queue_t));
-        if (aHash_q[i] == NULL) {
-            perror("Failed to allocate memory for hash queue");
-            return NULL;
-            for(size_t j = 0; j < i; ++j) {
-                free(aHash_q[j]);
-            }
-        }
-        aHash_q[i]->p_list_head_array = create_list();
-        if (aHash_q[i]->p_list_head_array == NULL) {
-            puts("Failed to create list");
-            return NULL;
-        }
-        if(aHash_q[i]->p_list_head_array == NULL){
-            puts("null occured");
-        }
-        aHash_q[i]->bucket_size = bucket_size;
+    printf("\nHash Queue Node Removing..\n");
+    for(index_t index = 0; index < KEY+1; ++index){
+        status = remove_key(hash_queue, index);
+
+        if(status == TRUE)
+            printf("KEY = [%llu] Removed\n", index);
+        else
+            printf("KEY = [%llu] is not Removed\n", index);
     }
-    return aHash_q;
+
+    printf("\nHash Queue Display\n");
+    show_hash_queue(hash_queue);
+
+    printf("\nHash Queue Destroyed\n");
+    destroy_hash_queue(hash_queue);
+
+    puts("show hash_Queue");
+    show_hash_queue(hash_queue);
+
+    puts("succeded");
+    return (0);
 }
 
-void add_key(hash_queue_t** p_hash_queue, key_t key){
-    size_t bucket_size;
-    size_t index;
+static void destroy_list(list_t** list){
 
-    bucket_size = (*p_hash_queue)[0].bucket_size;
-    index = hash(key,bucket_size);
-    list_t* head_node = NULL;
-
-    head_node = p_hash_queue[index]->p_list_head_array;
-    insert_end(head_node, key);
-    return;
-}
-
-// status_t remove_key(hash_queue_t* p_hash_queue, key_t key){
-
-// }
-
-// status_t search_key(hash_queue_t* p_hash_queue, key_t key){
-
-// }
-
-void show_hash_queue(hash_queue_t** p_hash_queue){
-
-    puts("printing all hash queue");
-    for(size_t i =0; i < p_hash_queue[0]->bucket_size; ++i){
-        printf("Queue = %llu\n", i);
-        if(p_hash_queue[i]->p_list_head_array == NULL)
-            assert(p_hash_queue[i]->p_list_head_array != NULL);
-
-        show_list(p_hash_queue[i]->p_list_head_array);
-    }
-}
-
-status_t destroy_hash_queue(hash_queue_t*** p_hash_queue){
-    size_t b = ((*p_hash_queue)[0])->bucket_size;
-
-    for(size_t i = 0; i < b; ++i){
-        list_t* dhash_q = NULL;
-        dhash_q = ((*p_hash_queue)[i])->p_list_head_array;
-        destroy(dhash_q);
-        ((*p_hash_queue)[i]) = NULL;
-    }
-        free((*p_hash_queue));
-        *p_hash_queue = NULL;
-    return(EXIT_SUCCESS);
-}
-
-void destroy(list_t* l){
-    list_t* run = NULL;
+    list_t* ptrArray = NULL;
     list_t* run_prev = NULL;
+    list_t* run = NULL;
+    ptrArray = *list;
+    
+    run = ptrArray->next;
 
-    run = l;
-    while(run != l){
-
+    while(run != ptrArray){
         run_prev = run;
-        run = run->next;
         free(run_prev);
-        run_prev = NULL;
+        run = run->next;
+    }
+    free(*list);
+    *list = NULL;
+}
+
+void destroy_hash_queue(hash_queue_t* hash_queue){
+
+    for(index_t index = 0; index < hash_queue->bucket_size; ++index){
+        destroy_list(&hash_queue->p_list_array[index]);
     }
 }
-/* Linked list interface functions */
-list_t* create_list(void){
+static list_t* find_search_node(list_t* list, key_t key){
+    list_t* run = NULL;
+    run = list->next;
+    while(run != list){
+        if(run->key == key)
+            return run;
 
-    list_t* p_node = NULL;
-    p_node = get_node(0);
-    assert(p_node!=NULL);
-    return p_node;
-}
-
-status_t insert_end(list_t* p_list, key_t key){
-  if (p_list == NULL) {
-        fprintf(stderr, "Error: List is NULL\n");
-        return FALSE;
+        run = run->next;
     }
+    return NULL;
+}
+status_t generic_delete(list_t* p_beg, list_t* p_mid, list_t* p_end){
 
-    node_t* new_node = get_node(key);
-    if (new_node == NULL) {
-        fprintf(stderr, "Error: Failed to create new node\n");
-        return FALSE;
+    p_beg->next = p_end;
+    p_end->prev = p_beg;
+
+    free(p_mid);
+    p_mid = NULL;
+
+    return(TRUE);
+}
+status_t remove_node(list_t* list, key_t key){
+    list_t* r_node = NULL;
+    r_node = find_search_node(list, key);
+
+    if(r_node != NULL){
+        return(generic_delete(r_node->prev, r_node, r_node->next));
     }
-    generic_insert(p_list->prev, get_node(key), p_list);
-    return(SUCCESS);
+    return(FALSE);
 }
-
-// status_t remove_key(list_t* p_list, key_t key){
-
-// }
-
-// status_t find(list_t* p_list, key_t key){
-
-// }
-
-/* Linked list helper functions */
-static void generic_insert(node_t* p_beg, node_t* p_mid, node_t* p_end){
-
-    if (p_beg == NULL || p_mid == NULL || p_end == NULL) {
-        puts("Invalid nodes for insertion");
-        return;
+status_t remove_key(hash_queue_t* hash_queue, key_t key){
+    status_t status = FALSE;
+    for(index_t index = 0; index < hash_queue->bucket_size; ++index){
+        status = remove_node(hash_queue->p_list_array[index], key);
+        if(status == TRUE)
+            return(TRUE);
     }
-    p_mid->prev = p_beg;
-    p_mid->next = p_end;
-    p_beg->next = p_mid;
-    p_end->prev = p_mid;
+    return (FALSE);
 }
 
-// static void generic_delete(node_t* p_delete_node){
+status_t search_node(list_t* list, key_t key){
 
-// }
+    list_t* run = NULL;
+    status_t status = FALSE;
 
-// static node_t* search_node(list_t* p_list, key_t key){
+    run = list->next;
 
-// }
-
-static node_t* get_node(key_t key){
-
-    node_t* p_node = NULL;
-    p_node = (node_t*)xmalloc(sizeof(node_t));
-    if (p_node == NULL) {
-        perror("Failed to allocate memory for node");
-        return NULL;
+    while(run != list){
+        if(run->key == key)       
+            return TRUE;
+        run = run->next;
     }
-    p_node->key = key;
-    p_node->next = p_node;
-    p_node->prev = p_node;
-    return (p_node);
+    return FALSE;
 }
 
-static void* xmalloc(size_t size_in_bytes){
+status_t search_key(hash_queue_t* hash_queue, key_t key){
+    status_t status = FALSE;
 
-    void* dMem = NULL;
-    dMem = malloc(size_in_bytes);
-    assert(dMem != NULL);
-    return dMem;
+    for(index_t index = 0; index < hash_queue->bucket_size; ++index){
+        status = search_node(hash_queue->p_list_array[index], key);
+
+        if(status == TRUE)
+            return status;
+    }
+    return status;
 }
 
-static index_t hash(key_t key, size_t bucket_size){
-    return (key % bucket_size); 
-}
-
-void show_list(list_t* p_node){
+void show_list(list_t* list){
     list_t* run = NULL;
 
-    if(p_node == NULL)
-        puts("NULL");
-
-    run = p_node->next;
-    printf("[head_node]->");
-    while(run != p_node){
+    run = list->next;
+    printf("[HEAD]->");
+    while(run != list){
+        
         printf("[%llu]->", run->key);
         run = run->next;
     }
-    puts("[END]\n");
+    printf("[END]\n");
+}
+
+index_t hash(key_t key, size_t bucket_size){
+    return (key % bucket_size);
+}
+void show_hash_queue(hash_queue_t* hash_queue){
+
+    if(*(hash_queue->p_list_array) == NULL){
+        puts("list is empty");
+        return;
+    }
+
+    for(index_t index = 0; index < hash_queue->bucket_size; ++index){
+        printf("Queue bucket[%llu]\t\t", index);
+        show_list(hash_queue->p_list_array[index]);
+    }
+}
+
+void insert_end(node_t* list, key_t key){
+    generic_insert(list->prev, get_node(key), list);
+}
+
+static void generic_insert(list_t* p_beg, list_t* p_mid, list_t* p_end){
+
+    p_mid->next = p_end;
+    p_mid->prev = p_beg;
+    p_beg->next = p_mid;
+    p_end->prev = p_mid; 
+}
+
+void add_key(hash_queue_t* hash_queue, key_t key){
+    index_t index;
+    index = hash(key, hash_queue->bucket_size); 
+    insert_end(hash_queue->p_list_array[index], key);
+}
+
+hash_queue_t* create_hash_queue(size_t bucket_size){
+
+    hash_queue_t* hash_queue = NULL;
+    hash_queue = (hash_queue_t*)malloc(sizeof(hash_queue_t));
+    assert(hash_queue !=NULL);
+    hash_queue->bucket_size = bucket_size;
+    hash_queue->p_list_array = (node_t**)malloc(bucket_size * sizeof(node_t*));
+    assert(hash_queue->p_list_array != NULL);
+
+    //Adding head node to each list 
+
+    for(index_t index = 0; index < bucket_size; ++index){
+        hash_queue->p_list_array[index] = create_list();    
+    }
+    return hash_queue;
+}
+
+static list_t* create_list(){
+    list_t* list_node = get_node(0);
+    return list_node;
+}
+
+static list_t* get_node(key_t key){
+    list_t* new_node = NULL;
+    new_node = (list_t*)xmalloc(sizeof(list_t));
+    new_node->key = key;
+    new_node->next = new_node;
+    new_node->prev = new_node;
+    return new_node;
+}
+
+static void* xmalloc(size_t size_in_byte){
+    void* v_ptr = NULL;
+    v_ptr = malloc(size_in_byte);
+    assert(v_ptr != NULL);
+    return v_ptr;
 }
